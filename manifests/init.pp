89cb1447cbc3965fc2d5e224a0e5c2dbdd213f19
Saz <me@saz.sh>
2011-06-15 18:39:58 +0200

    Changed to parameterized class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,3 @@
-class snmpd {
+class snmpd($ro_community = 'public', $rw_community = 'private', $ro_network = '127.0.0.1', $rw_network = '127.0.0.1', $location = 'Unknown', $contact = 'Unknown') {
     include snmpd::params, snmpd::install, snmpd::config, snmpd::service
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -7,34 +7,4 @@ class snmpd::params {
             $default_config = '/etc/default/snmpd'
         }
     }
-
-    $ro_community = $snmpd_ro_community ? {
-        ''      => "public",
-        default => $snmpd_ro_community,
-    }
-
-    $rw_community = $snmpd_rw_community ? {
-        ''      => "private",
-        default => $snmpd_rw_community,
-    }
-
-    $ro_network = $snmpd_ro_network ? {
-        ''      => "127.0.0.1",
-        default => $snmpd_ro_network,
-    }
-
-    $rw_network = $snmpd_rw_network ? {
-        ''      => "127.0.0.1",
-        default => $snmpd_rw_network,
-    }
-
-    $location = $snmpd_location ? {
-        ''      => "Unknown",
-        default => $snmpd_location,
-    }
-
-    $contact = $snmpd_contact ? {
-        ''      => "Unknown",
-        default => $snmpd_contact,
-    }
 }
