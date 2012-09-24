ef772b36c60ce11be557069a2ff5f829c9404ccb
Steffen Zieger <me@saz.sh>
2012-09-24 11:26:20 +0200

    Add initial FreeBSD support

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,17 +3,19 @@ class snmpd::config {
     owner   => 'root',
     group   => 'root',
     mode    => '0640',
-    content => template('snmpd/snmpd.conf.erb'),
+    content => template("snmpd/${snmpd::params::service_config_template}"),
     require => Class['snmpd::install'],
     notify  => Class['snmpd::service'],
   }
 
-  file { $snmpd::params::default_config:
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0640',
-    source  => 'puppet:///modules/snmpd/default_snmpd',
-    require => Class['snmpd::install'],
-    notify  => Class['snmpd::service'],
+  if $snmpd::params::default_config {
+    file { $snmpd::params::default_config:
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0640',
+      source  => "puppet:///modules/snmpd/${snmpd::params::default_config_source}"),
+      require => Class['snmpd::install'],
+      notify  => Class['snmpd::service'],
+    }
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,24 @@
 class snmpd::params {
   case $::osfamily {
-    'Debian': {
+    debian: {
       $package_name = 'snmpd'
       $service_config = '/etc/snmp/snmpd.conf'
+      $service_config_template = 'debian/snmpd.conf.erb'
       $service_name = 'snmpd'
       $default_config = '/etc/default/snmpd'
+      $default_config_source = 'debian/default_snmpd'
+    }
+    freebsd: {
+      $package_name = 'net-mgmt/bsnmp-ucd'
+      $service_config = '/etc/snmp/snmpd.conf'
+      $service_name = 'bsnmpd'
+    }
+  }
+  default: {
+    case $::operatingsystem {
+    }
+    default: {
+      fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
     }
   }
 }
